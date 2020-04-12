f0dbe46d6bb92bcf75cdb68aba82b85b8c124410
Igor Stockiy <shipa988@gmail.com>
2020-04-12 21:35:52 +0300

    hw05 completed

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -11,6 +11,33 @@ import (
 )
 
 func TestRun(t *testing.T) {
+	t.Run("border cases", func(t *testing.T) {
+		var runTasksCount int32
+		tCases := []struct {
+			name string
+			N    int
+			M    int
+			lenT int
+			err  error
+		}{
+			{"len(tests)<N", 10, 1, 5, nil},
+			{"N<=0", 0, 1, 5, fmt.Errorf("at least one goroutine and one possible error is needed to complete tasks")},
+			{"M<=0", 1, 0, 5, fmt.Errorf("at least one goroutine and one possible error is needed to complete tasks")},
+			{"no tasks", 10, 1, 0, nil},
+		}
+		for _, tCase := range tCases {
+			tasks := make([]Task, 0, tCase.lenT)
+			for i := 0; i < tCase.lenT; i++ {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+					atomic.AddInt32(&runTasksCount, 1)
+					return fmt.Errorf("error from task %d", i)
+				})
+			}
+			result := Run(tasks, tCase.N, tCase.M)
+			require.Equalf(t, tCase.err, result, tCase.name)
+		}
+	})
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -30,8 +57,7 @@ func TestRun(t *testing.T) {
 		result := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Equal(t, ErrErrorsLimitExceeded, result)
-		require.LessOrEqual(t,
-			int32(workersCount+maxErrorsCount), runTasksCount, "extra tasks were started")
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
