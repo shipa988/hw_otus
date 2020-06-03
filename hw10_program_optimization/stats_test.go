54e2374de88bf0432be93d3453e14e9c7092bea4
Igor Stockiy <shipa988@gmail.com>
2020-06-03 15:18:54 +0300

    hw10 complete

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -9,6 +9,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestGetDomainStatBad(t *testing.T) {
+	data1 := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`
+	data2 := `{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmithBrowsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Tek@list.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+	data3 := `{"Id:3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmithBrowsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Tek@list.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+	t.Run("return error in email with 2 @", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(data1), "gov")
+		require.Error(t, err)
+	})
+
+	t.Run("return error in email without @", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(data2), "com")
+		require.Error(t, err)
+	})
+
+	t.Run("return unmarshal error", func(t *testing.T) {
+		_, err := GetDomainStat(bytes.NewBufferString(data3), "com")
+		require.Error(t, err)
+	})
+}
+
 func TestGetDomainStat(t *testing.T) {
 	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
 {"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
